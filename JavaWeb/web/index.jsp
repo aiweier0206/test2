49de388f148cabfc81a5bc27faf4551f1d3037ab
aiweier0206 <m15546300772@163.com>
2019-04-01 15:10:22 +0800

    Initial commit

---
+++ JavaWeb/web/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 15546
+  Date: 2019/3/27
+  Time: 14:01
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>官网</title>
+  </head>
+  <body>
+  <h1 style="color: blue ">标题</h1>
+
+  </body>
+</html>
---
+++ src/Hello.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+
+/**
+ * @author yinhaicheng
+ * @create 2019-03-26-17:47
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        String[] s = new String[]{"s", "q", "e"};
+        for (int i = 0; i <s.length; i++) {
+            System.out.println(s[i]);
+        }
+
+        for (String s1 : s) {
+            
+        }
+        ArrayList<Object> list = new ArrayList<>();
+        list.add(s);
+
+        for (Object o : list) {
+            
+        }
+
+        if (list == null) {
+
+        }
+
+        if (list == null) {
+
+        }
+        if (list != null) {
+
+        }
+        if (list == null) {
+
+        }
+
+
+
+    }
+
+    /** 名称*/
+    private String name;
+
+
+}
